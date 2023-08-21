8c55fb885f79d36b6b656d8ab435f9075e4d2676
vkanimea <vkanimea@gmail.com>
2023-08-22 01:36:20 +1200

    aider: Removed duplicate function `RemoveSharingFileAccess` and added parameters `InputFile` and `ReportOutput` to `set-sp-rp.ps1`.
    # Aider chat conversation:
    
    USER: For the set-sp-rp.ps1 please remove lines 26 - 50 as this is not required. Add in the parameters for InputFile and ReportOutput.
    
    ASSISTANT: To make the requested changes, we will modify the `set-sp-rp.ps1` file.
    
    Here are the steps:
    
    1. Remove the duplicate function `RemoveSharingFileAccess` from lines 26 to 50.
    2. Add the parameters `InputFile` and `ReportOutput` to the parameters list.
    
    Here is the *edit block* for the changes:
    
    ```powershell
    set-sp-rp.ps1
    <<<<<<< HEAD
    # Function to remove sharing file access
    function RemoveSharingFileAccess($Item) {
        if ($Item.FileSystemObjectType -eq "File") {
            if ($Item.FieldValues["FileRef"] -ne $null -and $Item.FieldValues["FileRef"] -ne "") {
                $fileUrl = $Item.FieldValues["FileRef"]
                $fileName = $Item.FieldValues["FileLeafRef"]
                $destinationUrl = "$SiteUrl/$DestinationLibrary/$fileName"
                Copy-PnPFile -SourceUrl $fileUrl -TargetUrl $destinationUrl -Force
                Copy-PnPFile -SourceUrl $destinationUrl -TargetUrl $fileUrl -Force
            }
        }
    }
    #Parameters
    param(
        [Parameter(Mandatory=$true)]
        [string]$SiteUrl,
        [Parameter(Mandatory=$true)]
        [string]$SL,
        [Parameter(Mandatory=$false)]
        [string]$InputFile,
        [Parameter(Mandatory=$false)]
        [string]$ReportOutput = "report.csv"
    )
    =======
    #Parameters
    param(
        [Parameter(Mandatory=$true)]
        [string]$SiteUrl,
        [Parameter(Mandatory=$true)]
        [string]$SL,
        [Parameter(Mandatory=$true)]
        [string]$DL,
        [Parameter(Mandatory=$false)]
        [bool]$RemoveSharingFileAccess = $false,
        [Parameter(Mandatory=$false)]
        [string]$InputFile,
        [Parameter(Mandatory=$false)]
        [string]$ReportOutput = "report.csv"
    )
    >>>>>>> updated
    ```

--- set-sp-rp.ps1
+++ set-sp-rp.ps1
@@ -24,24 +24,16 @@ function RemoveSharingFileAccess($Item) {
 }
 
 # Rest of the code that calls RemoveSharingFileAccess and handles exceptions
-# Function to remove sharing file access
-function RemoveSharingFileAccess($Item) {
-    if ($Item.FileSystemObjectType -eq "File") {
-        if ($Item.FieldValues["FileRef"] -ne $null -and $Item.FieldValues["FileRef"] -ne "") {
-            $fileUrl = $Item.FieldValues["FileRef"]
-            $fileName = $Item.FieldValues["FileLeafRef"]
-            $destinationUrl = "$SiteUrl/$DestinationLibrary/$fileName"
-            Copy-PnPFile -SourceUrl $fileUrl -TargetUrl $destinationUrl -Force
-            Copy-PnPFile -SourceUrl $destinationUrl -TargetUrl $fileUrl -Force
-        }
-    }
-}
 #Parameters
 param(
     [Parameter(Mandatory=$true)]
     [string]$SiteUrl,
     [Parameter(Mandatory=$true)]
     [string]$SL,
+    [Parameter(Mandatory=$true)]
+    [string]$DL,
+    [Parameter(Mandatory=$false)]
+    [bool]$RemoveSharingFileAccess = $false,
     [Parameter(Mandatory=$false)]
     [string]$InputFile,
     [Parameter(Mandatory=$false)]
